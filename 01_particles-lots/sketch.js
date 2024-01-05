ba59c80ec37c4de3276a2778cad8b3d32fd481e8
Christensen-John <121368802+Christensen-John@users.noreply.github.com>
2024-01-05 12:02:17 -0600

    Add images to preview, Add volume reponse to particles

--- 01_particles-lots/sketch.js
+++ 01_particles-lots/sketch.js
@@ -1,16 +1,25 @@
 const CANVASWIDTH = 500;
 const CANVASHEIGHT = 500;
 let local_canvas;
+let mic;
+let startButton = document.querySelector(".btn");
+let currAudio;
 
 function setup() {
 	local_canvas = createCanvas(CANVASWIDTH, CANVASHEIGHT);
 	local_canvas.parent("particle-sketch");
+
+	mic = new p5.AudioIn();
+	mic.start();
+
+	noLoop();
 }
 
 function draw() {
 	if (mouseIsPressed) {
 		background(255, 50);
 	}
+
 	// background(255, 50);
 	let randLoopControl = random(50, 101);
 	for (let i = 0; i < randLoopControl; i++) {
@@ -24,6 +33,29 @@ function draw() {
 		let randGreenVal = random(0, 255);
 		let randBlueVal = random(0, 255);
 		fill(randRedVal, randGreenVal, randBlueVal);
-		circle(randXLoc, randYLoc, randDiameter);
+
+		let micLevel = mic.getLevel();
+
+		let mappedLevel = mapMicInput(micLevel, 3, 20);
+		console.log(micLevel);
+		console.log(mappedLevel);
+		circle(randXLoc, randYLoc, mappedLevel);
+	}
+}
+
+function mapMicInput(num, outMin, outMax) {
+	let inMin = 0;
+	let inMax = 0.1;
+	return ((num - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+}
+
+function keyPressed() {
+	if (keyCode === ENTER) {
+		console.log("Enter was pressed!");
+		getAudioContext().resume();
+		loop();
+		draw();
+	} else {
+		noLoop();
 	}
 }
